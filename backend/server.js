8bc4dda07979bf3ec90c42348d3323ada2263095
Suman Kumar Jana <108161316+suman272001@users.noreply.github.com>
2024-06-16 13:03:42 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,11 @@ const express=require("express");
 const route=require("./Router/route");
 const cors=require("cors");
 const app=express();
-app.use(cors())
+app.use(cors({
+    origin:["https://full-stack-to-do-list-api.vercel.app"],
+    methods:["POST","GET"],
+    credentials:true
+}))
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 app.use(route);
